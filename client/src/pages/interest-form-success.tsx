4d85922acbee393f3ceec4d89eb3d7ba042ee00b
Ezhumalai Castellanre <ezhumalai@castellanre.com>
2025-09-02 18:40:48 +0530

    add

--- client/src/pages/interest-form-success.tsx
+++ client/src/pages/interest-form-success.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card, CardContent } from '../components/ui/card'
 import { Button } from '../components/ui/button'
 import { CheckCircle, Home } from 'lucide-react'
@@ -6,6 +6,23 @@ import { useLocation } from 'wouter'
 
 export default function InterestFormSuccess() {
   const [, setLocation] = useLocation()
+
+  // Fire TikTok Pixel conversion on thank-you page
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+    const win = window as any
+
+    if (win.ttq && typeof win.ttq.track === 'function') {
+      try { win.ttq.track('SubmitForm') } catch {}
+      return
+    }
+
+    const bootstrap = document.createElement('script')
+    bootstrap.type = 'text/javascript'
+    bootstrap.async = true
+    bootstrap.text = `!function (w, d, t) {\n  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(\nvar e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")\n;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};\n\n\n  ttq.load('D2R9TUBC77UCDUAMLM7G');\n  ttq.page();\n  ttq.track('SubmitForm');\n}(window, document, 'ttq');`
+    document.head.appendChild(bootstrap)
+  }, [])
 
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
